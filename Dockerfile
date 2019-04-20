80300e2bcab41d9902b175c6f0b663dcc551fbea
paulfantom <pawel@krupa.net.pl>
2019-04-20 15:43:24 +0200

    init

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM arm32v7/golang:stretch
+
+COPY qemu-arm-static /usr/bin/
+WORKDIR /go/src/github.com/automatedhome/roomtemp
+COPY . .
+RUN go build -o roomtemp cmd/main.go
+
+FROM arm32v7/busybox:1.30-glibc
+
+COPY --from=0 /go/src/github.com/automatedhome/roomtemp/roomtemp /usr/bin/roomtemp
+
+ENTRYPOINT [ "/usr/bin/roomtemp" ]
---
+++ cmd/main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+
+	mqttclient "github.com/automatedhome/flow-meter/pkg/mqttclient"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+type ScheduleCell struct {
+	From        string  `json:"from"`
+	To          string  `json:"to"`
+	Temperature float64 `json:"temperature"`
+}
+
+type Schedule struct {
+	Workday            []ScheduleCell `json:"workday"`
+	Freeday            []ScheduleCell `json:"freeday"`
+	DefaultTemperature float64        `json:"defaultTemperature"`
+}
+
+type BoolPoint struct {
+	v    bool
+	addr string
+}
+
+type DataPoint struct {
+	v    float64
+	addr string
+}
+
+type Sensors struct {
+	holiday  BoolPoint
+	override DataPoint
+}
+
+type Actuators struct {
+	expected DataPoint
+}
+
+var sensors Sensors
+var actuators Actuators
+var schedule Schedule
+var overrideEnd time.Time
+var scheduleTopic string
+var client mqtt.Client
+
+func onMessage(client mqtt.Client, message mqtt.Message) {
+	switch message.Topic() {
+	case sensors.holiday.addr:
+		value, err := strconv.ParseBool(string(message.Payload()))
+		if err != nil {
+			log.Printf("Received incorrect message payload: '%v'\n", message.Payload())
+			return
+		}
+		sensors.holiday.v = value
+
+	case sensors.override.addr:
+		overrideEnd = time.Now().Add(time.Duration(60 * time.Minute))
+		value, err := strconv.ParseFloat(string(message.Payload()), 64)
+		if err != nil {
+			log.Printf("Received incorrect message payload: '%v'\n", message.Payload())
+			return
+		}
+		sensors.override.v = value
+
+	case scheduleTopic:
+		var tmp = Schedule{}
+		err := json.Unmarshal(message.Payload(), &tmp)
+		if err != nil {
+			log.Printf("Received incorrect message payload: '%v'\n", message.Payload())
+			return
+		}
+		schedule = tmp
+	}
+}
+
+func stringToDate(str string) time.Time {
+	now := time.Now()
+	t := strings.Split(str, ":")
+	h, _ := strconv.Atoi(t[0])
+	m, _ := strconv.Atoi(t[1])
+	return time.Date(now.Year(), now.Month(), now.Day(), h, m, 0, 0, time.Local)
+}
+
+func setExpected(value float64) {
+	if actuators.expected.v != value {
+		client.Publish(actuators.expected.addr, 0, false, fmt.Sprintf("%.2f", value))
+		actuators.expected.v = value
+		log.Printf("Setting expected temperature to %.2f", value)
+	}
+}
+
+func init() {
+	sensors.holiday = BoolPoint{false, "heater/settings/holiday"}
+	sensors.override = DataPoint{18, "heater/settings/override"}
+	actuators.expected = DataPoint{18, "heater/settings/expected"}
+	schedule.DefaultTemperature = 0
+	scheduleTopic = "heater/settings/schedule"
+
+	overrideEnd = time.Now()
+}
+
+func main() {
+	broker := flag.String("broker", "tcp://127.0.0.1:1883", "The full url of the MQTT server to connect to ex: tcp://127.0.0.1:1883")
+	clientID := flag.String("clientid", "heater", "A clientid for the connection")
+	flag.Parse()
+
+	brokerURL, _ := url.Parse(*broker)
+	var topics []string
+	topics = append(topics, sensors.holiday.addr, sensors.override.addr, scheduleTopic)
+	client = mqttclient.New(*clientID, brokerURL, topics, onMessage)
+	log.Printf("Connected to %s as %s and waiting for messages\n", *broker, *clientID)
+
+	// Wait for sensors data
+	for {
+		if schedule.DefaultTemperature == 0 {
+			break
+		}
+		log.Println("Waiting 15s for schedule data...")
+		time.Sleep(15 * time.Second)
+	}
+	log.Printf("Starting with schedule received: %+v\n", schedule)
+
+	// run program
+	var cells []ScheduleCell
+	for {
+		time.Sleep(1 * time.Second)
+
+		// check if manual override heating mode is enabled
+		if time.Now().Before(overrideEnd) {
+			setExpected(sensors.override.v)
+			continue
+		}
+
+		// check if now is the time to start heating
+		if sensors.holiday.v {
+			cells = schedule.Freeday
+		} else {
+			cells = schedule.Workday
+		}
+		for _, cell := range cells {
+			from := stringToDate(cell.From)
+			to := stringToDate(cell.To)
+			if time.Now().After(from) && time.Now().Before(to) {
+				setExpected(cell.Temperature)
+				continue
+			}
+		}
+		setExpected(schedule.DefaultTemperature)
+	}
+}
